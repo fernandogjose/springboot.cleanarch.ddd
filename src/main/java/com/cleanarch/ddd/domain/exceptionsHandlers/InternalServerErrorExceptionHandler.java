95affe78b16244c18e01bcad717837d2f8a6dc23
Fernando Jose <fernando.jose@luizalabs.com>
2024-03-21 11:00:30 -0300

    refactor: melhorando as exceptions

---
+++ src/main/java/com/cleanarch/ddd/domain/exceptionsHandlers/InternalServerErrorExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.cleanarch.ddd.domain.exceptionsHandlers;
+
+import com.cleanarch.ddd.domain.exceptions.ResponseException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+@ControllerAdvice
+@RestController
+public class InternalServerErrorExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<ResponseException> handleAllExceptions(Exception ex, WebRequest request) {
+        ResponseException responseException = new ResponseException(
+                new Date(), ex.getMessage(), request.getDescription(false)
+        );
+
+        return new ResponseEntity<>(responseException, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
